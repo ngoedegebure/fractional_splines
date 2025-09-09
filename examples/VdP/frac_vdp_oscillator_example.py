899599670228868addc93547b93e472fc5af8e9e
Niels Goedegebure <nielsgoedegebure@gmail.com>
2025-09-09 16:53:58 +0800

    Small updates

--- examples/VdP/frac_vdp_oscillator_example.py
+++ examples/VdP/frac_vdp_oscillator_example.py
@@ -21,53 +21,52 @@ Enjoy the stability, quasiperiodicity and chaos!
 # VdP function setup #
 ######################
 
-alpha_damping = 0.8     # Fractional order of damping
-beta = 1              # Hilfer Beta
+alpha_damping = 0.8  # Fractional order of damping
+beta = 1  # Hilfer Beta
 
-params = {
-    'mu': 1     # mu parameter of VdP oscillator
-}
+params = {"mu": 1}  # mu parameter of VdP oscillator
 
 # Forcing is a list of dictionaries applying A*sin(omega*t) + c to solution component dim (1 gives y in this case)
-forcing_params = [{
-    'dim' : 1,
-    'A' : 3, #3
-    'omega' : 3.3,#1.94,#4.0,#6.2,
-    'c' : 0
-}]
+forcing_params = [{"dim": 1, "A": 3, "omega": 3.3, "c": 0}]  # 3  # 1.94,#4.0,#6.2,
 
 # Initialize the function for the Van der Pol oscillator
 # This has to be done before the next steps in order to get the right N_upscale. See below.
-VdP_bs = fr.ode_functions.VdP(params, bernstein = True, transpose = False) 
+VdP_bs = fr.ode_functions.VdP(params, bernstein=True, transpose=False)
 
 ###################
 # Spline settings #
 ###################
 
 ### Knot input values ###
-T = 100                 # Integration max time
-eps = 10**(-15)         # Time shift epsilon, start of interval
-dt = 0.05               # Spline size (also called h) though varying size can also be used by creating a custom t_knot_vals
-c = 3/2                 # Knot size increase constant
+T = 100  # Integration max time
+eps = 10 ** (-15)  # Time shift epsilon, start of interval
+dt = 0.05  # Spline size (also called h) though varying size can also be used by creating a custom t_knot_vals
+c = 3 / 2  # Knot size increase constant
 ##
 
-gamm = alpha_damping + beta - alpha_damping*beta        # Hilfer gamma parametrization
-t_knot_vals = build_hilf_knot_vals(eps, T, c, gamm, dt) # Knot values: equidistant for Caputo (beta = 1)
+gamm = alpha_damping + beta - alpha_damping * beta  # Hilfer gamma parametrization
+t_knot_vals = build_hilf_knot_vals(
+    eps, T, c, gamm, dt
+)  # Knot values: equidistant for Caputo (beta = 1)
 
 ####
 
-n_eval = 1 # Polynomial order
+n_eval = 1  # Polynomial order
 
 # A bit of a technicality: the order of the polynomial goes up by 3 since it gets multiplied 3 times in the VdP system equation.
 # In general equal to m * n_eval, where m denotes the highest order of multiplication in x-components of f(x)
 n_calc = VdP_bs.N_upscale * n_eval
 
-bs = BernsteinSplines(t_knot_vals, n_calc, n_eval=n_eval, eq_opt=True)  # Initialize splines setup!
+bs = BernsteinSplines(
+    t_knot_vals, n_calc, n_eval=n_eval, eq_opt=True
+)  # Initialize splines setup!
 
-mult = bs.splines_multiply   # Bernstein multiplication method
-upscale = bs.splines_upscale # Bernstein upscale method to match the polynomial order (same as multiplying n times with identity splines of the same order)
+mult = bs.splines_multiply  # Bernstein multiplication method
+upscale = (
+    bs.splines_upscale
+)  # Bernstein upscale method to match the polynomial order (same as multiplying n times with identity splines of the same order)
 
-VdP_bs.set_bs_mult_upscale_functions(mult, upscale)  
+VdP_bs.set_bs_mult_upscale_functions(mult, upscale)
 
 ############
 # Compute! #
@@ -77,56 +76,54 @@ VdP_bs.set_bs_mult_upscale_functions(mult, upscale)
 x_0 = np.array([0, 0])
 
 # alpha for x and y component separately. For reference, see https://doi.org/10.1016/j.chaos.2006.05.010
-system_alpha_vals = [alpha_damping, 2-alpha_damping]    
+system_alpha_vals = [alpha_damping, 2 - alpha_damping]
 # Initialize the solving structure
-spline_solver = bs.initialize_solver(VdP_bs.f, x_0, system_alpha_vals, beta_vals = beta, forcing_params = forcing_params) 
+spline_solver = bs.initialize_solver(
+    VdP_bs.f, x_0, system_alpha_vals, beta_vals=beta, forcing_params=forcing_params
+)
 # Run the solver. method = 'local' uses knot-by-knot integration, method = 'global' at-large interval integration.
-results = spline_solver.run(method='local', verbose = False, t_eval=t_knot_vals)
+results = spline_solver.run(method="local", verbose=False, t_eval=t_knot_vals)
 
 # Save some results...
-x = results['x'][0]                   # Function at evaluation points as vector
-a_vals = results['a']                 # Splines coefficients in matrix form
-comp_time = results['total_time']     # Total computational time
-t = results['t']                      # Knot time values
+x = results["x"][0]  # Function at evaluation points as vector
+a_vals = results["a"]  # Splines coefficients in matrix form
+comp_time = results["total_time"]  # Total computational time
+t = results["t"]  # Knot time values
 
-x_der = np.diff(x)/np.diff(t) # Calculate the derivative of x
+x_der = np.diff(x) / np.diff(t)  # Calculate the derivative of x
 
 ############
 # Plotting #
 ############
 
-skip_n_vals = 20 # Skip first n vals in case of Hilfer derivative for plotting
-if np.all(beta == 1) :
+skip_n_vals = 20  # Skip first n vals in case of Hilfer derivative for plotting
+if np.all(beta == 1):
     skip_n_vals = 0
 
-plot_x = x[skip_n_vals:-1] # Last value is discarded for the derivative calculation
+plot_x = x[skip_n_vals:-1]  # Last value is discarded for the derivative calculation
 plot_x_der = x_der[skip_n_vals:]
 plot_t = t[skip_n_vals:-1]
 
 # If provided, can be used to keep fixed limits for the frame. Currently not inputted below.
-lims_override = {
-    'x': [-2.0, 2.0],
-    'xder': [-5.0, 5.0],
-    'fourier_amp': [0, 2.8]
-}
+lims_override = {"x": [-2.0, 2.0], "xder": [-5.0, 5.0], "fourier_amp": [0, 2.8]}
 
 plot_object = VdP_Plotter(
-    plot_x, 
-    plot_x_der, 
-    plot_t, 
-    params, 
-    alpha_damping, 
-    dt, 
-    T, 
-    n_eval, 
-    comp_time, 
-    forcing_params=forcing_params, 
-    lims_override = None,
-    beta = beta
+    plot_x,
+    plot_x_der,
+    plot_t,
+    params,
+    alpha_damping,
+    dt,
+    T,
+    n_eval,
+    comp_time,
+    forcing_params=forcing_params,
+    lims_override=None,
+    beta=beta,
 )
 
 plot_object.phase()
-plot_object.phase(empty = True)
+plot_object.phase(empty=True)
 plot_object.threedee()
 plot_object.fourier_spectrum()
 plot_object.phase_fourier()
--- fracnum/ode_functions.py
+++ fracnum/ode_functions.py
@@ -1,6 +1,7 @@
 from fracnum.splines.backend import np
 from abc import ABC, abstractmethod
 
+
 class ODEFunctions(ABC):
     def __init__(self, params, bernstein, transpose):
         self.params = params
@@ -16,8 +17,9 @@ class ODEFunctions(ABC):
     def f(self, t_vals, x_vals):
         pass
 
+
 class VdP(ODEFunctions):
-    def __init__(self, params = {}, bernstein=False, transpose = False):
+    def __init__(self, params={}, bernstein=False, transpose=False):
         super().__init__(params, bernstein, transpose)
         self.N_upscale = 3
         self.three_system = False
@@ -26,7 +28,7 @@ class VdP(ODEFunctions):
         if self.transpose:
             x_vals = x_vals.T
 
-        mu = self.params['mu']
+        mu = self.params["mu"]
         if self.bernstein:
             # breakpoint()
             A_x_in, A_y_in = x_vals[0, :], x_vals[1, :]
@@ -36,12 +38,16 @@ class VdP(ODEFunctions):
                 A_x_out = self.bs_upscale(A_y_in, self.N_upscale)
 
                 # y' = mu * (1-x^2)y - x
-                A_y_out = mu * self.bs_mult(1 - self.bs_mult(A_x_in, A_x_in), A_y_in) - self.bs_upscale(A_x_in, self.N_upscale)
-                
-                if 'A' in self.params and 'omega' in self.params:
-                    if self.params['A'] !=0 and self.params['omega'] != 0:
+                A_y_out = mu * self.bs_mult(
+                    1 - self.bs_mult(A_x_in, A_x_in), A_y_in
+                ) - self.bs_upscale(A_x_in, self.N_upscale)
+
+                if "A" in self.params and "omega" in self.params:
+                    if self.params["A"] != 0 and self.params["omega"] != 0:
                         # breakpoint()
-                        A_y_out += self.params['A']*np.sin(self.params['omega']*t_vals)
+                        A_y_out += self.params["A"] * np.sin(
+                            self.params["omega"] * t_vals
+                        )
 
                 return np.array([A_x_out, A_y_out])
             else:
@@ -51,37 +57,40 @@ class VdP(ODEFunctions):
                 A_x_out = self.bs_upscale(A_z_in, self.N_upscale)
 
                 # y' = mu * (1-x^2)z - x
-                A_y_out = mu * self.bs_mult(1 - self.bs_mult(A_x_in, A_x_in), A_z_in) - self.bs_upscale(A_x_in, self.N_upscale)
+                A_y_out = mu * self.bs_mult(
+                    1 - self.bs_mult(A_x_in, A_x_in), A_z_in
+                ) - self.bs_upscale(A_x_in, self.N_upscale)
 
                 # z' = y
                 A_z_out = self.bs_upscale(A_y_in, self.N_upscale)
 
                 return np.array([A_x_out, A_y_out, A_z_out])
-        else: # The non-Bernstein spline one is added for quicker comparison options with e.g. Diethelm's method (see fracnum.numerical)
+        else:  # The non-Bernstein spline one is added for quicker comparison options with e.g. Diethelm's method (see fracnum.numerical)
             x, y = x_vals[0], x_vals[1]
             # x' = y
             x_out = y
             # y' = mu * (1-x^2)y - x
-            y_out = -x - mu * (x**2 -1)*y
+            y_out = -x - mu * (x**2 - 1) * y
 
             # Forcing included here to use in e.g. Diethelm or another numerical piece
-            if 'A' in self.params and 'omega' in self.params:
-                A = float(self.params['A'])
-                omega = float(self.params['omega'])
+            if "A" in self.params and "omega" in self.params:
+                A = float(self.params["A"])
+                omega = float(self.params["omega"])
                 if A != 0 and omega != 0:
                     # TODO: CHANGE TO GENERAL ELEMENT ADDITION
-                    y_out+=A*np.sin(omega*t_vals)
+                    y_out += A * np.sin(omega * t_vals)
 
-            if 'c' in self.params.keys():
-                c = self.params['c']
+            if "c" in self.params.keys():
+                c = self.params["c"]
                 if c != 0:
                     # TODO: CHANGE TO GENERAL ELEMENT ADDITION
-                    y_out+=c
+                    y_out += c
 
             return np.array([x_out, y_out]).T
-                    
+
+
 class NegExp(ODEFunctions):
-    def __init__(self, params = {}, bernstein=False, transpose = False):
+    def __init__(self, params={}, bernstein=False, transpose=False):
         super().__init__(params, bernstein, transpose)
         self.N_upscale = 1
 
@@ -95,41 +104,44 @@ class NegExp(ODEFunctions):
             A_x_out = -A_x_in
 
             return np.array([A_x_out])
-        else: # The non-Bernstein spline one is added for quicker comparison options with e.g. Diethelm's method (see fracnum.numerical)
+        else:  # The non-Bernstein spline one is added for quicker comparison options with e.g. Diethelm's method (see fracnum.numerical)
             x, y = x_vals[0]
 
             x_out = x
 
             return np.array([x_out]).T
-        
+
+
 class t_k(ODEFunctions):
-    def __init__(self, params = {}, bernstein=False, transpose = False):
+    def __init__(self, params={}, bernstein=False, transpose=False):
         super().__init__(params, bernstein, transpose)
         self.N_upscale = 1
 
     def f(self, t_vals, x_vals):
-        k = np.array(self.params['k'])
-        if 'c' in self.params.keys():
-            c = self.params['c']
+        k = np.array(self.params["k"])
+        if "c" in self.params.keys():
+            c = self.params["c"]
         else:
             c = 0
-        res = np.array([np.sum([t_vals**k_i for k_i in k], axis=0) + c])
+        res = t_vals**k + c
 
         if self.transpose:
             return res.T
         else:
             return res
-    
+
+
 class test_fun(ODEFunctions):
-    def __init__(self, params = {}, bernstein=False, transpose = False):
+    def __init__(self, params={}, bernstein=False, transpose=False):
         super().__init__(params, bernstein, transpose)
         self.N_upscale = 1
 
     def f(self, t_vals, x_vals):
-        return np.array([t_vals**2 - (1-t_vals)**3])
-    
+        return np.array([t_vals**2 - (1 - t_vals) ** 3])
+
+
 class lin_damp_os(ODEFunctions):
-    def __init__(self, params = {}, bernstein=True, transpose = False):
+    def __init__(self, params={}, bernstein=True, transpose=False):
         super().__init__(params, bernstein, transpose)
         self.N_upscale = 1
 
@@ -137,7 +149,7 @@ class lin_damp_os(ODEFunctions):
         if self.transpose:
             x_vals = x_vals.T
 
-        eta, omega = self.params['eta'], self.params['omega']
+        eta, omega = self.params["eta"], self.params["omega"]
         if self.bernstein:
             A_x_in, A_y_in = x_vals[0, :], x_vals[1, :]
 
@@ -145,8 +157,102 @@ class lin_damp_os(ODEFunctions):
             A_x_out = A_y_in
 
             # y' = mu * (1-x^2)y - x
-            A_y_out = -eta*A_y_in -omega**2*A_x_in
+            A_y_out = -eta * A_y_in - omega**2 * A_x_in
 
             return np.array([A_x_out, A_y_out])
         else:
             assert 0
+
+
+class Infiltration(ODEFunctions):
+    def __init__(
+        self,
+        params={},
+        bernstein=False,
+        transpose=False,
+        M=None,
+        dz=None,
+        b=None,
+        T_z0=None,
+        T_zb=None,
+        P_z0=None,
+        P_zb=None,
+    ):
+
+        super().__init__(params, bernstein, transpose)
+        self.N_upscale = 2
+        self.M = M  # number of discretization points
+        self.dz = dz  # space discretization step
+        self.b = b  # end of space interval
+        self.T_z0 = lambda x: np.array([T_z0(x)])
+        self.T_zb = lambda x: np.array([T_zb(x)])
+        self.P_z0 = lambda x: np.array([P_z0(x)])
+        self.P_zb = lambda x: np.array([P_zb(x)])
+
+        self.k = float(self.params["k"])
+        self.beta = float(self.params["beta"])
+        self.chi = float(self.params["chi"])
+        self.h = float(self.params["h"])
+        self.alpha = float(self.params["alpha"])
+
+    def f_T_int(self, T_vals, P_vals):
+        k_part = self.k * self.bs_upscale(
+            T_vals[0] - 2 * T_vals[1] + T_vals[2], self.N_upscale
+        )
+        beta_part = (
+            self.beta / 4 * self.bs_mult(T_vals[2] - T_vals[0], P_vals[2] - P_vals[0])
+        )
+        chi_part = (
+            self.chi / 4 * self.bs_mult(P_vals[2] - P_vals[0], P_vals[2] - P_vals[0])
+        )
+        # breakpoint()
+        return (k_part + beta_part + chi_part) / (self.dz**2)
+
+    def f_P_int(self, T_vals, P_vals):
+        h_part = self.h * self.bs_upscale(
+            P_vals[0] - 2 * P_vals[1] + P_vals[2], self.N_upscale
+        )
+        a_k_part = (
+            self.alpha
+            * self.k
+            * self.bs_upscale(T_vals[0] - 2 * T_vals[1] + T_vals[2], self.N_upscale)
+        )
+        a_beta_part = (
+            self.alpha
+            * self.beta
+            / 4
+            * self.bs_mult(T_vals[2] - T_vals[0], P_vals[2] - P_vals[0])
+        )
+        a_chi_part = (
+            self.alpha
+            * self.chi
+            / 4
+            * self.bs_mult(P_vals[2] - P_vals[0], P_vals[2] - P_vals[0])
+        )
+        return (h_part + a_k_part + a_beta_part + a_chi_part) / (self.dz**2)
+
+    def f(self, t_vals, U):
+        N_dims = U.shape[0]
+        N_half = N_dims // 2
+        T_in, P_in = U[0:N_half], U[N_half::]
+        T_out, P_out = [None] * N_half, [None] * N_half
+        for i in range(N_half):
+            if i == 0:
+                # Start boundary point
+                T_vals_sel = [self.T_z0(t_vals), T_in[i], T_in[i + 1]]
+                P_vals_sel = [self.P_z0(t_vals), P_in[i], P_in[i + 1]]
+            elif i == (N_half - 1):
+                # End boundary point
+                # breakpoint()
+                T_vals_sel = [T_in[i - 1], T_in[i], self.T_zb(t_vals)]
+                P_vals_sel = [P_in[i - 1], P_in[i], self.P_zb(t_vals)]
+            else:
+                # Internal point
+                T_vals_sel = [T_in[i - 1], T_in[i], T_in[i + 1]]
+                P_vals_sel = [P_in[i - 1], P_in[i], P_in[i + 1]]
+
+            T_out[i] = self.f_T_int(T_vals_sel, P_vals_sel)
+            P_out[i] = self.f_P_int(T_vals_sel, P_vals_sel)
+
+        U_out = np.concatenate([T_out, P_out])
+        return U_out
